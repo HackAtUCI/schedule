c510f5bdbe9154969f92c8203b789f768038d1c7
chriswongtv <chris@cogifire.com>
2018-03-02 19:15:06 -0800

    Add config file and update README

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# schedule
-Simple, responsive schedule template for hackathons
+# Hackathon Schedule Template
+Simple, responsive schedule template for hackathons. Built on Angular.js, it takes in a schedule and displays it in a nicely formatted list.
+
+## Features
+* Reads in schedule data from a JSON file (you can even use an online JSON host such as [Jasonbase](https://www.jasonbase.com/) for fast editing).
+* Supports one-day, two-day and three-day schedules.
+* Displays a countdown timer that counts to both starting time and ending time.
+* Navigation bar for easier access to Devpost, Slack, etc.
+* Responsive, works on mobile!
+
+## Screenshots
+#### Desktop
+!(Desktop)[/screenshots/desktop.png]
+
+#### Desktop (Two-day Schedule)
+!(Desktop with two-day schedule)[/screenshots/desktop_dual.png]
+
+#### Mobile
+!(Mobile)[/screenshots/mobile.png]
+
+## Setup
+1. Clone the repository.
+2. Update the `startDate` and `endDate` variables in `config.json`.
+3. Generate your JSON schedule file based on the format found in the samples at `sample_schedules/` and assign it to the `scheduleUrl` variable in `config.json`.
+4. Update the branding.
+5. Update the URLs on the navbar.
+6. You’re good to go!
+
+## Contributing
+All contributions are welcome and appreciated!
--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 angular.module('app', [])
 	.controller('ScheduleCtrl', ['$scope', '$document', '$timeout', '$http', function($scope, $document, $timeout, $http) {
-    // Update values for countdown timer
-    var startDate = moment('March 3, 2018 01:00');
-    var endDate = moment('March 4, 2018 15:00');
+    $scope.schedule = [];
+
+    $http
+      .get('./config.json')
+      .then(function(res) {
+        if (window.location.hostname === 'schedule-demo.hackuci.com') {
+          $scope.startDate = moment().add(7.32, 'hours');
+        } else {
+          $scope.startDate = moment(res.data.startDate);
+        }
+        $scope.endDate = moment(res.data.endDate);
+        var scheduleUrl = res.data.scheduleUrl;
+
+        $scope.calcCountdown();
+
+        $http
+          .get(scheduleUrl)
+          .then(function(res) {
+            for (var day in res.data) {
+              $scope.schedule.push({ day: day, schedule: res.data[day] });
+            }
+          });
+      });
 
     $scope.calcCountdown = function() {
       var now = Date.now();
-      if (now <= startDate) {
+      if (now <= $scope.startDate) {
         // countdown to start
-        var diff = startDate - now;
+        var diff = $scope.startDate - now;
         $scope.days = Math.floor(diff / (1000 * 60 * 60 * 24));
         $scope.hours = (Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) + ($scope.days * 24);
         $scope.minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
@@ -23,9 +43,9 @@ angular.module('app', [])
         }
 
         $timeout($scope.calcCountdown, 1000);
-      } else if (now <= endDate) {
+      } else if (now <= $scope.endDate) {
         // countdown to end
-        var diff = endDate - now;
+        var diff = $scope.endDate - now;
         $scope.days = Math.floor(diff / (1000 * 60 * 60 * 24));
         $scope.hours = (Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))) + ($scope.days * 24);
         $scope.minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
@@ -49,8 +69,6 @@ angular.module('app', [])
       }
     }
 
-    $scope.calcCountdown();
-
     var originalSchedulePosition = document.getElementsByClassName('schedule-box')[0].getBoundingClientRect().top;
     var countdownPosition = document.getElementsByClassName('countdown-container')[0].getBoundingClientRect().top;
 
@@ -67,16 +85,6 @@ angular.module('app', [])
         })
       }
     });
-
-    $scope.schedule = [];
-
-    $http
-      .get('./three_day_schedule.json')
-      .then(function(res) {
-        for (var day in res.data) {
-          $scope.schedule.push({ day: day, schedule: res.data[day] });
-        }
-      });
 
     $('.navbar-mobile-menu-icon').on('click', function() {
       $('.navbar-modal')
